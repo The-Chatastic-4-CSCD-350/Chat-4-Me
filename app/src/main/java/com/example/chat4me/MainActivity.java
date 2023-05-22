e2340c5c26ff8c0a1bab076529399e499a5c615d
Joshua Merrell <jmerrell2@ewu.edu>
2023-05-21 20:56:49 -0700

    Add menu item to read SMS

--- app/src/main/java/com/example/chat4me/MainActivity.java
+++ app/src/main/java/com/example/chat4me/MainActivity.java
@@ -133,7 +133,7 @@ public class MainActivity extends AppCompatActivity
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_read_sms) {
             showSmsPermission();
             return true;
         }
