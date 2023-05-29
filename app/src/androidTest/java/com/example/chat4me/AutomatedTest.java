f9dd23ac2ce20ad862c0128a19c4b247de2930af
Joshua Merrell <jmerrell2@ewu.edu>
2023-05-29 14:45:32 -0700

    Add test for accepting disclaimer and permissions

--- app/src/androidTest/java/com/example/chat4me/AutomatedTest.java
+++ app/src/androidTest/java/com/example/chat4me/AutomatedTest.java
@@ -4,21 +4,24 @@ import androidx.test.ext.junit.runners.AndroidJUnit4;
 import static androidx.test.core.app.ApplicationProvider.getApplicationContext;
 
 import androidx.test.filters.SdkSuppress;
+import androidx.test.internal.platform.content.PermissionGranter;
 import androidx.test.platform.app.InstrumentationRegistry;
 
 import android.content.Context;
 import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.content.pm.ResolveInfo;
+import android.widget.TextView;
 
 import androidx.test.uiautomator.By;
 import androidx.test.uiautomator.UiDevice;
+import androidx.test.uiautomator.UiObject2;
 import androidx.test.uiautomator.Until;
 
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import java.util.List;
 
 @RunWith(AndroidJUnit4.class)
 @SdkSuppress(minSdkVersion = 18)
@@ -30,8 +33,8 @@ public class AutomatedTest {
     @Before
     public void startMainActivityFromHome() {
         device = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation());
+        Assert.assertNotNull(device);
         device.pressHome();
-
         // wait for launcher
         final String launcherPackage = device.getLauncherPackageName();
         device.wait(Until.hasObject(By.pkg(launcherPackage).depth(0)), LAUNCH_TIMEOUT);
@@ -40,7 +43,7 @@ public class AutomatedTest {
         Context context = getApplicationContext();
         final Intent intent = context.getPackageManager()
                 .getLaunchIntentForPackage(MAIN_PKG);
-        intent.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
         context.startActivity(intent);
 
         device.wait(Until.hasObject(By.pkg(MAIN_PKG)
@@ -48,9 +51,25 @@ public class AutomatedTest {
     }
 
     @Test
-    public void doTest() {
-        Assert.assertNotNull(device);
-        //Assert.assertNotNull(getLauncherPackageName());
-    }
+    public void promptVisibleTest() {
+        UiObject2 alertTitle = device.findObject(By.text("First-use disclaimer"));
+        Assert.assertNotNull(alertTitle);
 
+        UiObject2 promptOK = device.findObject(By.clazz("android.widget.Button")
+                .text("OK").clickable(true));
+        Assert.assertNotNull(promptOK);
+
+        promptOK.click();
+        device.wait(Until.gone(By.text("First-use disclaimer")), 500);
+
+        UiObject2 allowBtn = device.wait(
+                Until.findObject(By.clickable(true).text("Allow")),
+                500);
+        Assert.assertNotNull(allowBtn);
+        allowBtn.click();
+
+        // wait until permission dialog is gone after clicking Allow
+        device.wait(Until.gone(By.clickable(true).text("Allow")),
+                500);
+    }
 }
